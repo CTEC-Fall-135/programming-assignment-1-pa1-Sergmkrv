8d19f9c08116f6d1f952773e90b84e4d1fcbf7a3
Sergey Makarov <Sergmak9300@gmail.com>
2024-10-03 11:26:32 -0700

    Ready to grade

---
+++ Module1/WorkFlowDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Author: Sergey Makarov
+Date: 10/3/24
+Assignment: Programming Assignment 1
+*/
+
+namespace WorkFlowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" __         __");
+            Console.WriteLine("/  \\.-\"\"\"-./  \\");
+            Console.WriteLine("\\    -   -    /");
+            Console.WriteLine(" |   $   $   |");
+            Console.WriteLine(" \\  .-'''-.  /");
+            Console.WriteLine("  '-\\__Y__/-'");
+            Console.WriteLine("     `---`");
+        }
+    }
+}
